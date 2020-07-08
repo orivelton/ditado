fdbd2aca5e545d8b02fb6b6cdc218dc7e349e9ea
Orivelton Cesar <orivelton10@gmail.com>
2020-07-08 16:38:17 +0100

    chore: voice and reload list

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,10 +3,7 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import HearingIcon from '@material-ui/icons/Hearing';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import ReplayIcon from '@material-ui/icons/Replay';
 
 import Words from '../data/text.json';
 
@@ -25,58 +22,59 @@ const useStyles = makeStyles((theme) => ({
 
 const Play = () => {
   // const synth = window.speechSynthesis;
-  const [text, setText] = useState('');
   const [count, setCount] = useState(0);
-
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
+  const [showPlay, setShowPlay] = useState(true);
 
   const readText = () => {
     setCount(count + 1);
     console.log(count);
     
-    
     const msg = new SpeechSynthesisUtterance(Words[count].text);
     msg.lang = 'pt-PT';
     msg.voiceURI = "native";
+    msg.rate = 0.7;
     window.speechSynthesis.speak(msg);
+
+    if((Words.length -1) === count) {
+      setShowPlay(false);
+    }
     
   }
 
-
+  const reload = () => {
+    setCount(0);
+    setShowPlay(true);
+  }
 
   const classes = useStyles();
+
   return (
     <>
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        endIcon={<HearingIcon />}
-        onClick={readText}
-      >
-        Play
-      </ Button>
+      {
+        showPlay ? (
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            endIcon={<HearingIcon />}
+            onClick={readText}
+          >
+            Play
+          </ Button>
 
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Ditado</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={text}
-          onChange={handleChange}
-          label="Ditado"
-        >
-          <MenuItem value="">
-            <em>Selecione</em>
-          </MenuItem>
-          {
-            Words.map(({ text }) => <MenuItem key={text} value={text}>{text}</MenuItem>)
-          }
-          
-        </Select>
-      </FormControl>
+        ) : (
+          <Button
+            variant="contained"
+            size="large"
+            className={classes.button}
+            endIcon={<ReplayIcon />}
+            onClick={reload}
+          >
+            Reload
+          </ Button>
+        )
+      }
     </>
   )
 }
