743382f2ac2d3da0bc985b7379f48bfa316701d5
Orivelton Cesar <orivelton10@gmail.com>
2020-07-09 18:16:13 +0100

    chore: deploy

--- src/helpers/helper.js
+++
@@ -1,3 +0,0 @@
-const sortByProperty = array => array.sort((a,b) => (a.voiceURI > b.voiceURI) ? 1 : ((b.voiceURI > a.voiceURI) ? -1 : 0));
-
-export { sortByProperty }
--- src/settings/config.js
+++
@@ -1,9 +0,0 @@
-const speakText = (text) => {
-  const msg = new SpeechSynthesisUtterance(text);
-  msg.lang = localStorage.getItem('lang') || 'pt-PT';
-  msg.voiceURI = "native";
-  msg.rate = 0.7;
-  window.speechSynthesis.speak(msg);
-}
-
-export { speakText };
